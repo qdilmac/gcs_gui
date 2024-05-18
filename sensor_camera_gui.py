37bcd52b1d323146f9b0c414d79f701fce975732
qdilmac <shadeofficial7@outlook.com>
2024-05-18 20:50:07 +0300

    necessary updates to implement camera feed

--- sensor_camera_gui.py
+++ sensor_camera_gui.py
@@ -6,79 +6,103 @@
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt, QTimer)
+    QSize, QTime, QUrl, Qt, QTimer, QThread)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLCDNumber, QLabel, QMainWindow,
+from PySide6.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QLCDNumber, QLabel, QMainWindow,
     QMenuBar, QSizePolicy, QStatusBar, QWidget, QPushButton)
 from PySide6 import QtWidgets
 import time
 import serial
 from serial.tools import list_ports
 import sys
+import cv2
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(690, 423)
+        MainWindow.resize(960, 720)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QSize(960, 720))
+        MainWindow.setMaximumSize(QSize(960, 720))
+        font = QFont()
+        font.setBold(False)
+        MainWindow.setFont(font)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.accelxLCD = QLCDNumber(self.centralwidget)
         self.accelxLCD.setObjectName(u"accelxLCD")
-        self.accelxLCD.setGeometry(QRect(140, 30, 151, 41))
+        self.accelxLCD.setGeometry(QRect(240, 400, 151, 41))
         self.accelyLCD = QLCDNumber(self.centralwidget)
         self.accelyLCD.setObjectName(u"accelyLCD")
-        self.accelyLCD.setGeometry(QRect(140, 80, 151, 41))
+        self.accelyLCD.setGeometry(QRect(240, 450, 151, 41))
         self.accelzLCD = QLCDNumber(self.centralwidget)
         self.accelzLCD.setObjectName(u"accelzLCD")
-        self.accelzLCD.setGeometry(QRect(140, 130, 151, 41))
+        self.accelzLCD.setGeometry(QRect(240, 500, 151, 41))
         self.gyrozLCD = QLCDNumber(self.centralwidget)
         self.gyrozLCD.setObjectName(u"gyrozLCD")
-        self.gyrozLCD.setGeometry(QRect(460, 130, 151, 41))
+        self.gyrozLCD.setGeometry(QRect(560, 500, 151, 41))
         self.gyroxLCD = QLCDNumber(self.centralwidget)
         self.gyroxLCD.setObjectName(u"gyroxLCD")
-        self.gyroxLCD.setGeometry(QRect(460, 30, 151, 41))
+        self.gyroxLCD.setGeometry(QRect(560, 400, 151, 41))
         self.gyroyLCD = QLCDNumber(self.centralwidget)
         self.gyroyLCD.setObjectName(u"gyroyLCD")
-        self.gyroyLCD.setGeometry(QRect(460, 80, 151, 41))
+        self.gyroyLCD.setGeometry(QRect(560, 450, 151, 41))
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(90, 40, 49, 16))
+        self.label.setGeometry(QRect(190, 410, 49, 16))
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(90, 90, 49, 16))
+        self.label_2.setGeometry(QRect(190, 460, 49, 16))
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(90, 140, 49, 16))
+        self.label_3.setGeometry(QRect(190, 510, 49, 16))
         self.label_4 = QLabel(self.centralwidget)
         self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(410, 40, 49, 16))
+        self.label_4.setGeometry(QRect(510, 410, 49, 16))
         self.label_5 = QLabel(self.centralwidget)
         self.label_5.setObjectName(u"label_5")
-        self.label_5.setGeometry(QRect(410, 90, 49, 16))
+        self.label_5.setGeometry(QRect(510, 460, 49, 16))
         self.label_6 = QLabel(self.centralwidget)
         self.label_6.setObjectName(u"label_6")
-        self.label_6.setGeometry(QRect(410, 140, 49, 16))
-        
-        # ON/OFF butonları
-        self.ledOnButton = QPushButton(self.centralwidget)
+        self.label_6.setGeometry(QRect(510, 510, 49, 16))
+        self.VideoFeedLabel = QLabel(self.centralwidget)
+        self.VideoFeedLabel.setObjectName(u"VideoFeedLabel")
+        self.VideoFeedLabel.setGeometry(QRect(30, 20, 891, 331))
+        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(350, 560, 221, 111))
+        self.horizontalLayoutWidget = QWidget(self.groupBox)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(30, 20, 160, 80))
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.ledOnButton = QPushButton(self.horizontalLayoutWidget)
         self.ledOnButton.setObjectName(u"ledOnButton")
-        self.ledOnButton.setGeometry(QRect(250, 240, 81, 81))
         self.ledOnButton.clicked.connect(self.ledOn) # -> ledOn fonksiyonu çağrılıyor
-       
-        self.ledOffButton = QPushButton(self.centralwidget)
+        self.horizontalLayout.addWidget(self.ledOnButton)
+
+        self.ledOffButton = QPushButton(self.horizontalLayoutWidget)
         self.ledOffButton.setObjectName(u"ledOffButton")
-        self.ledOffButton.setGeometry(QRect(360, 240, 81, 81))
-        self.ledOffButton.clicked.connect(self.ledOff) # -> ledOff fonksiyonu çağrılıyor
-        
+        self.ledOffButton.clicked.connect(self.ledOff)
+        self.horizontalLayout.addWidget(self.ledOffButton) # -> ledOff fonksiyonu çağrılıyor
+
+        self.label_8 = QLabel(self.centralwidget)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setGeometry(QRect(0, 0, 961, 20))
+        font1 = QFont()
+        font1.setBold(True)
+        self.label_8.setFont(font1)
+        self.label_8.setAlignment(Qt.AlignCenter)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(MainWindow)
-        self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 690, 22))
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -89,15 +113,19 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MPU6050 GUI ile Veri Okuma", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Sensor/Camera Data Monitoring v0.1", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"AccelX", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"AccelY", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"AccelZ", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"GyroX", None))
         self.label_5.setText(QCoreApplication.translate("MainWindow", u"GyroY", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"GyroZ", None))
+        self.VideoFeedLabel.setText("")
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Led Control", None))
         self.ledOnButton.setText(QCoreApplication.translate("MainWindow", u"ON", None))
         self.ledOffButton.setText(QCoreApplication.translate("MainWindow", u"OFF", None))
+        self.label_8.setStyleSheet(QCoreApplication.translate("MainWindow", u"0", None))
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"Video Feed", None))
     # retranslateUi
     
     # serial porttan veri okuma fonksiyonu
