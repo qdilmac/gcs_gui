586a9352e0b8da73545dd2a9239fbf6bd92a29b5
qdilmac <shadeofficial7@outlook.com>
2024-05-30 21:19:39 +0300

    added roll, pitch, yaw readings

---
+++ version_two/gcs_gui_v2.ino
@@ -0,0 +1,112 @@
+// Written by Mustafa Osman Dilmaç
+// 30/05/2024 21:10
+
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <math.h>
+
+#define LED_one 25
+#define LED_two 26
+#define LED_three 27
+
+Adafruit_MPU6050 mpu;
+
+// yaw verisi almak biraz sıkıntı ama olsun :d
+float yaw = 0.0;
+unsigned long lastUpdateTime = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(1000);
+  while (!Serial) {
+    delay(10);
+  }
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  pinMode(LED_one, OUTPUT);
+  pinMode(LED_two, OUTPUT);
+  pinMode(LED_three, OUTPUT);
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.println("");
+  delay(100);
+}
+
+void loop() {
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  // Yatış (roll), Yunuslama (pitch), Sapma (Yaw) hesaplamaları
+  float roll = atan2(a.acceleration.y, a.acceleration.z) * 180 / PI;
+  float pitch = atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180 / PI;
+
+  // Geçen zaman hesabı -> bu yüzden yaw hesabı biraz sıkıntılı
+  unsigned long currentTime = millis();
+  float deltaTime = (currentTime - lastUpdateTime) / 1000.0; // milisaniye -> saniye
+  lastUpdateTime = currentTime;
+
+  yaw += g.gyro.z * deltaTime;
+  yaw = fmod(yaw, 360.0); // Veri sapması 360 dereceden büyük olmamalı -> Roll-Yaw-Pitch hesaplamalarına kalman filtresi eklenecek
+
+  /* Print out the values */
+  Serial.print(a.acceleration.x);
+  Serial.print(" ");
+  Serial.print(a.acceleration.y);
+  Serial.print(" ");
+  Serial.print(a.acceleration.z);
+  Serial.print(" ");
+  Serial.print(g.gyro.x);
+  Serial.print(" ");
+  Serial.print(g.gyro.y);
+  Serial.print(" ");
+  Serial.print(g.gyro.z);
+  Serial.print(" ");
+  // önce pitch verisini gönderiyorum. Bunun tek sebebi mpu6050'nin breadboard üzerinde takılış şekli. 
+  // mpu6050 sensörünün eksenel yönlerine göre veriyi gönderiyorum
+  // sensör üzerinde x,y,z olarak göstermiş, GUI'nın veri de ona göre okuması için sıralama bu şekilde
+  Serial.print(pitch);  
+  Serial.print(" ");
+  Serial.print(roll);
+  Serial.print(" ");
+  Serial.print(yaw);  
+  Serial.println();
+  delay(1000);
+
+  // LED kontrol serial port üzerinden
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    switch (command) {
+      case '1':
+        digitalWrite(LED_one, HIGH);
+        break;
+      case '2':
+        digitalWrite(LED_one, LOW);
+        break;
+      case '3':
+        digitalWrite(LED_two, HIGH);
+        break;
+      case '4':
+        digitalWrite(LED_two, LOW);
+        break;
+      case '5':
+        digitalWrite(LED_three, HIGH);
+        break;
+      case '6':
+        digitalWrite(LED_three, LOW);
+        break;
+      default:
+        // Unknown command
+        break;
+    }
+  }
+}
--- version_two/main.py
+++ version_two/main.py
@@ -41,28 +41,117 @@ class Ui_MainWindow(object):
         self.videofeed_label.setGeometry(QRect(40, 40, 640, 480))
         self.groupBox = QGroupBox(self.centralwidget)
         self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(690, 30, 321, 321))
+        self.groupBox.setGeometry(QRect(690, 30, 321, 351))
         font = QFont()
         font.setBold(True)
         self.groupBox.setFont(font)
         self.gridLayoutWidget = QWidget(self.groupBox)
         self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
-        self.gridLayoutWidget.setGeometry(QRect(10, 30, 301, 281))
+        self.gridLayoutWidget.setGeometry(QRect(10, 30, 301, 311))
         self.gridLayout_2 = QGridLayout(self.gridLayoutWidget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.line_9 = QFrame(self.gridLayoutWidget)
+        self.line_9.setObjectName(u"line_9")
+        self.line_9.setFrameShape(QFrame.HLine)
+        self.line_9.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_9, 14, 1, 1, 1)
+
+        self.gyrox_label = QLabel(self.gridLayoutWidget)
+        self.gyrox_label.setObjectName(u"gyrox_label")
+
+        self.gridLayout_2.addWidget(self.gyrox_label, 7, 1, 1, 1)
+
+        self.label_5 = QLabel(self.gridLayoutWidget)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setFont(font)
+
+        self.gridLayout_2.addWidget(self.label_5, 15, 0, 1, 1)
+
+        self.line_14 = QFrame(self.gridLayoutWidget)
+        self.line_14.setObjectName(u"line_14")
+        self.line_14.setFrameShape(QFrame.HLine)
+        self.line_14.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_14, 4, 1, 1, 1)
+
+        self.accelx_label = QLabel(self.gridLayoutWidget)
+        self.accelx_label.setObjectName(u"accelx_label")
+
+        self.gridLayout_2.addWidget(self.accelx_label, 1, 1, 1, 1)
+
+        self.line_3 = QFrame(self.gridLayoutWidget)
+        self.line_3.setObjectName(u"line_3")
+        self.line_3.setFrameShape(QFrame.HLine)
+        self.line_3.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_3, 4, 0, 1, 1)
+
+        self.label_10 = QLabel(self.gridLayoutWidget)
+        self.label_10.setObjectName(u"label_10")
+        self.label_10.setFont(font)
+
+        self.gridLayout_2.addWidget(self.label_10, 9, 0, 1, 1)
+
+        self.line_8 = QFrame(self.gridLayoutWidget)
+        self.line_8.setObjectName(u"line_8")
+        self.line_8.setFrameShape(QFrame.HLine)
+        self.line_8.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_8, 14, 0, 1, 1)
+
+        self.label_2 = QLabel(self.gridLayoutWidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setFont(font)
+
+        self.gridLayout_2.addWidget(self.label_2, 1, 0, 1, 1)
+
+        self.pitchangle_label = QLabel(self.gridLayoutWidget)
+        self.pitchangle_label.setObjectName(u"pitchangle_label")
+
+        self.gridLayout_2.addWidget(self.pitchangle_label, 15, 1, 1, 1)
+
+        self.gyroy_label = QLabel(self.gridLayoutWidget)
+        self.gyroy_label.setObjectName(u"gyroy_label")
+
+        self.gridLayout_2.addWidget(self.gyroy_label, 9, 1, 1, 1)
+
+        self.yaw_angle = QLabel(self.gridLayoutWidget)
+        self.yaw_angle.setObjectName(u"yaw_angle")
+
+        self.gridLayout_2.addWidget(self.yaw_angle, 17, 1, 1, 1)
+
+        self.line_12 = QFrame(self.gridLayoutWidget)
+        self.line_12.setObjectName(u"line_12")
+        self.line_12.setFrameShape(QFrame.HLine)
+        self.line_12.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_12, 8, 1, 1, 1)
+
+        self.accely_label = QLabel(self.gridLayoutWidget)
+        self.accely_label.setObjectName(u"accely_label")
+
+        self.gridLayout_2.addWidget(self.accely_label, 3, 1, 1, 1)
+
+        self.line_10 = QFrame(self.gridLayoutWidget)
+        self.line_10.setObjectName(u"line_10")
+        self.line_10.setFrameShape(QFrame.HLine)
+        self.line_10.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_10, 12, 1, 1, 1)
+
         self.label_4 = QLabel(self.gridLayoutWidget)
         self.label_4.setObjectName(u"label_4")
         self.label_4.setFont(font)
 
         self.gridLayout_2.addWidget(self.label_4, 3, 0, 1, 1)
 
-        self.line_5 = QFrame(self.gridLayoutWidget)
-        self.line_5.setObjectName(u"line_5")
-        self.line_5.setFrameShape(QFrame.HLine)
-        self.line_5.setFrameShadow(QFrame.Sunken)
+        self.label_12 = QLabel(self.gridLayoutWidget)
+        self.label_12.setObjectName(u"label_12")
+        self.label_12.setFont(font)
 
-        self.gridLayout_2.addWidget(self.line_5, 8, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.label_12, 11, 0, 1, 1)
 
         self.line_4 = QFrame(self.gridLayoutWidget)
         self.line_4.setObjectName(u"line_4")
@@ -78,16 +167,6 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.line_7, 12, 0, 1, 1)
 
-        self.gyrox_label = QLabel(self.gridLayoutWidget)
-        self.gyrox_label.setObjectName(u"gyrox_label")
-
-        self.gridLayout_2.addWidget(self.gyrox_label, 7, 1, 1, 1)
-
-        self.rollangle_label = QLabel(self.gridLayoutWidget)
-        self.rollangle_label.setObjectName(u"rollangle_label")
-
-        self.gridLayout_2.addWidget(self.rollangle_label, 13, 1, 1, 1)
-
         self.line_2 = QFrame(self.gridLayoutWidget)
         self.line_2.setObjectName(u"line_2")
         self.line_2.setFrameShape(QFrame.HLine)
@@ -95,133 +174,22 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.line_2, 2, 1, 1, 1)
 
-        self.accelz_label = QLabel(self.gridLayoutWidget)
-        self.accelz_label.setObjectName(u"accelz_label")
-
-        self.gridLayout_2.addWidget(self.accelz_label, 5, 1, 1, 1)
-
-        self.label_3 = QLabel(self.gridLayoutWidget)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setFont(font)
-
-        self.gridLayout_2.addWidget(self.label_3, 13, 0, 1, 1)
-
-        self.label_10 = QLabel(self.gridLayoutWidget)
-        self.label_10.setObjectName(u"label_10")
-        self.label_10.setFont(font)
-
-        self.gridLayout_2.addWidget(self.label_10, 9, 0, 1, 1)
-
-        self.line = QFrame(self.gridLayoutWidget)
-        self.line.setObjectName(u"line")
-        self.line.setFrameShape(QFrame.HLine)
-        self.line.setFrameShadow(QFrame.Sunken)
-
-        self.gridLayout_2.addWidget(self.line, 2, 0, 1, 1)
-
-        self.accelx_label = QLabel(self.gridLayoutWidget)
-        self.accelx_label.setObjectName(u"accelx_label")
-
-        self.gridLayout_2.addWidget(self.accelx_label, 1, 1, 1, 1)
-
-        self.gyroz_label = QLabel(self.gridLayoutWidget)
-        self.gyroz_label.setObjectName(u"gyroz_label")
-
-        self.gridLayout_2.addWidget(self.gyroz_label, 11, 1, 1, 1)
-
-        self.label_5 = QLabel(self.gridLayoutWidget)
-        self.label_5.setObjectName(u"label_5")
-        self.label_5.setFont(font)
-
-        self.gridLayout_2.addWidget(self.label_5, 15, 0, 1, 1)
-
-        self.accely_label = QLabel(self.gridLayoutWidget)
-        self.accely_label.setObjectName(u"accely_label")
-
-        self.gridLayout_2.addWidget(self.accely_label, 3, 1, 1, 1)
-
-        self.label_2 = QLabel(self.gridLayoutWidget)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setFont(font)
-
-        self.gridLayout_2.addWidget(self.label_2, 1, 0, 1, 1)
-
-        self.line_6 = QFrame(self.gridLayoutWidget)
-        self.line_6.setObjectName(u"line_6")
-        self.line_6.setFrameShape(QFrame.HLine)
-        self.line_6.setFrameShadow(QFrame.Sunken)
-
-        self.gridLayout_2.addWidget(self.line_6, 10, 0, 1, 1)
-
         self.label_6 = QLabel(self.gridLayoutWidget)
         self.label_6.setObjectName(u"label_6")
         self.label_6.setFont(font)
 
         self.gridLayout_2.addWidget(self.label_6, 5, 0, 1, 1)
 
-        self.label_12 = QLabel(self.gridLayoutWidget)
-        self.label_12.setObjectName(u"label_12")
-        self.label_12.setFont(font)
+        self.rollangle_label = QLabel(self.gridLayoutWidget)
+        self.rollangle_label.setObjectName(u"rollangle_label")
 
-        self.gridLayout_2.addWidget(self.label_12, 11, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.rollangle_label, 13, 1, 1, 1)
 
-        self.line_3 = QFrame(self.gridLayoutWidget)
-        self.line_3.setObjectName(u"line_3")
-        self.line_3.setFrameShape(QFrame.HLine)
-        self.line_3.setFrameShadow(QFrame.Sunken)
+        self.label_3 = QLabel(self.gridLayoutWidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setFont(font)
 
-        self.gridLayout_2.addWidget(self.line_3, 4, 0, 1, 1)
-
-        self.label_7 = QLabel(self.gridLayoutWidget)
-        self.label_7.setObjectName(u"label_7")
-        self.label_7.setFont(font)
-
-        self.gridLayout_2.addWidget(self.label_7, 7, 0, 1, 1)
-
-        self.pitchangle_label = QLabel(self.gridLayoutWidget)
-        self.pitchangle_label.setObjectName(u"pitchangle_label")
-
-        self.gridLayout_2.addWidget(self.pitchangle_label, 15, 1, 1, 1)
-
-        self.gyroy_label = QLabel(self.gridLayoutWidget)
-        self.gyroy_label.setObjectName(u"gyroy_label")
-
-        self.gridLayout_2.addWidget(self.gyroy_label, 9, 1, 1, 1)
-
-        self.line_8 = QFrame(self.gridLayoutWidget)
-        self.line_8.setObjectName(u"line_8")
-        self.line_8.setFrameShape(QFrame.HLine)
-        self.line_8.setFrameShadow(QFrame.Sunken)
-
-        self.gridLayout_2.addWidget(self.line_8, 14, 0, 1, 1)
-
-        self.line_9 = QFrame(self.gridLayoutWidget)
-        self.line_9.setObjectName(u"line_9")
-        self.line_9.setFrameShape(QFrame.HLine)
-        self.line_9.setFrameShadow(QFrame.Sunken)
-
-        self.gridLayout_2.addWidget(self.line_9, 14, 1, 1, 1)
-
-        self.line_10 = QFrame(self.gridLayoutWidget)
-        self.line_10.setObjectName(u"line_10")
-        self.line_10.setFrameShape(QFrame.HLine)
-        self.line_10.setFrameShadow(QFrame.Sunken)
-
-        self.gridLayout_2.addWidget(self.line_10, 12, 1, 1, 1)
-
-        self.line_11 = QFrame(self.gridLayoutWidget)
-        self.line_11.setObjectName(u"line_11")
-        self.line_11.setFrameShape(QFrame.HLine)
-        self.line_11.setFrameShadow(QFrame.Sunken)
-
-        self.gridLayout_2.addWidget(self.line_11, 10, 1, 1, 1)
-
-        self.line_12 = QFrame(self.gridLayoutWidget)
-        self.line_12.setObjectName(u"line_12")
-        self.line_12.setFrameShape(QFrame.HLine)
-        self.line_12.setFrameShadow(QFrame.Sunken)
-
-        self.gridLayout_2.addWidget(self.line_12, 8, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.label_3, 13, 0, 1, 1)
 
         self.line_13 = QFrame(self.gridLayoutWidget)
         self.line_13.setObjectName(u"line_13")
@@ -230,16 +198,73 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.line_13, 6, 1, 1, 1)
 
-        self.line_14 = QFrame(self.gridLayoutWidget)
-        self.line_14.setObjectName(u"line_14")
-        self.line_14.setFrameShape(QFrame.HLine)
-        self.line_14.setFrameShadow(QFrame.Sunken)
+        self.line_11 = QFrame(self.gridLayoutWidget)
+        self.line_11.setObjectName(u"line_11")
+        self.line_11.setFrameShape(QFrame.HLine)
+        self.line_11.setFrameShadow(QFrame.Sunken)
 
-        self.gridLayout_2.addWidget(self.line_14, 4, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.line_11, 10, 1, 1, 1)
+
+        self.line_5 = QFrame(self.gridLayoutWidget)
+        self.line_5.setObjectName(u"line_5")
+        self.line_5.setFrameShape(QFrame.HLine)
+        self.line_5.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_5, 8, 0, 1, 1)
+
+        self.line = QFrame(self.gridLayoutWidget)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line, 2, 0, 1, 1)
+
+        self.label_7 = QLabel(self.gridLayoutWidget)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setFont(font)
+
+        self.gridLayout_2.addWidget(self.label_7, 7, 0, 1, 1)
+
+        self.accelz_label = QLabel(self.gridLayoutWidget)
+        self.accelz_label.setObjectName(u"accelz_label")
+
+        self.gridLayout_2.addWidget(self.accelz_label, 5, 1, 1, 1)
+
+        self.gyroz_label = QLabel(self.gridLayoutWidget)
+        self.gyroz_label.setObjectName(u"gyroz_label")
+
+        self.gridLayout_2.addWidget(self.gyroz_label, 11, 1, 1, 1)
+
+        self.label_8 = QLabel(self.gridLayoutWidget)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setFont(font)
+
+        self.gridLayout_2.addWidget(self.label_8, 17, 0, 1, 1)
+
+        self.line_6 = QFrame(self.gridLayoutWidget)
+        self.line_6.setObjectName(u"line_6")
+        self.line_6.setFrameShape(QFrame.HLine)
+        self.line_6.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_6, 10, 0, 1, 1)
+
+        self.line_17 = QFrame(self.gridLayoutWidget)
+        self.line_17.setObjectName(u"line_17")
+        self.line_17.setFrameShape(QFrame.HLine)
+        self.line_17.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_17, 16, 0, 1, 1)
+
+        self.line_18 = QFrame(self.gridLayoutWidget)
+        self.line_18.setObjectName(u"line_18")
+        self.line_18.setFrameShape(QFrame.HLine)
+        self.line_18.setFrameShadow(QFrame.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_18, 16, 1, 1, 1)
 
         self.groupBox_2 = QGroupBox(self.centralwidget)
         self.groupBox_2.setObjectName(u"groupBox_2")
-        self.groupBox_2.setGeometry(QRect(690, 360, 321, 161))
+        self.groupBox_2.setGeometry(QRect(690, 400, 321, 161))
         self.groupBox_2.setFont(font)
         self.gridLayoutWidget_2 = QWidget(self.groupBox_2)
         self.gridLayoutWidget_2.setObjectName(u"gridLayoutWidget_2")
@@ -455,29 +480,32 @@ class Ui_MainWindow(object):
         self.gyrox_label.setText(str(data[3]))
         self.gyroy_label.setText(str(data[4]))
         self.gyroz_label.setText(str(data[5]))
-        # self.rollangle_label.setText(data[6])  # -> Henüz eklemedim
-        # self.pitchangle_label.setText(data[7])
+        self.rollangle_label.setText(str(data[6]))
+        self.pitchangle_label.setText(str(data[7]))
+        self.yaw_angle.setText(str(data[8]))
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Data Monitoring GUI v0.2", None))
         self.videofeed_label.setText("")
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Sensor Data - READ", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"AccelY", None))
         self.gyrox_label.setText("")
-        self.rollangle_label.setText("")
-        self.accelz_label.setText("")
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Roll Angle", None))
-        self.label_10.setText(QCoreApplication.translate("MainWindow", u"GyroY", None))
-        self.accelx_label.setText("")
-        self.gyroz_label.setText("")
         self.label_5.setText(QCoreApplication.translate("MainWindow", u"Pitch Angle", None))
-        self.accely_label.setText("")
+        self.accelx_label.setText("")
+        self.label_10.setText(QCoreApplication.translate("MainWindow", u"GyroY", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"AccelX", None))
-        self.label_6.setText(QCoreApplication.translate("MainWindow", u"AccelZ", None))
-        self.label_12.setText(QCoreApplication.translate("MainWindow", u"GyroZ", None))
-        self.label_7.setText(QCoreApplication.translate("MainWindow", u"GyroX", None))
         self.pitchangle_label.setText("")
         self.gyroy_label.setText("")
+        self.yaw_angle.setText("")
+        self.accely_label.setText("")
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"AccelY", None))
+        self.label_12.setText(QCoreApplication.translate("MainWindow", u"GyroZ", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"AccelZ", None))
+        self.rollangle_label.setText("")
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Roll Angle", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"GyroX", None))
+        self.accelz_label.setText("")
+        self.gyroz_label.setText("")
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"Yaw Angle", None))
         self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"Sensor Data - WRITE", None))
         self.led2_button.setText(QCoreApplication.translate("MainWindow", u"Led 2", None))
         self.led1_button.setText(QCoreApplication.translate("MainWindow", u"Led 1", None))
